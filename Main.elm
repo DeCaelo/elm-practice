10f91cf63b15e0c7a9e0b8f8b2288b8d04332405
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-04-15 11:01:17 +0200

    elm-sequencing-data-with-lists-in-elm wtf cette synthaxe

--- Main.elm
+++ Main.elm
@@ -1,23 +1,57 @@
 module Main exposing (..)
 
 import Html exposing (text)
+import List
 
-politely : String -> String
-politely phrase =
-    "Excuse me, " ++ phrase 
+-- politely : String -> String
+-- politely phrase =
+--     "Excuse me, " ++ phrase 
 
-ask : String -> String -> String
-ask thing place =
-    "is there a "
-        ++ thing
-        ++ " in the "
-        ++ place
-        ++ "?"
+-- ask : String -> String -> String
+-- ask thing place =
+--     "is there a "
+--         ++ thing
+--         ++ " in the "
+--         ++ place
+--         ++ "?"
 
-askPolitelyAboutFish : String -> String
-askPolitelyAboutFish = 
-    politely << (ask "fish")
+-- askPolitelyAboutFish : String -> String
+-- askPolitelyAboutFish = 
+--     politely << (ask "fish")
+
+-- main = 
+--     text <| askPolitelyAboutFish "hat"
+
+type alias Person = 
+    { name: String
+    , age: Int 
+    }
+
+people = 
+    [ { name = "Legolas", age = 2931 }
+    , { name = "Gimli", age = 139 }
+    ]
+
+names: List Person -> List String
+names peeps = 
+    List.map (\peep -> peep.name) peeps
+
+findPerson : String -> List Person -> Maybe Person
+findPerson name peeps = List.foldl
+    (\peep memo -> 
+        case memo of
+            Just _ -> 
+                memo
+            
+            Nothing -> 
+                if peep.name == name then
+                    Just peep
+                else 
+                    Nothing
+        )
+        Nothing
+        peeps
 
 main = 
-    text <| askPolitelyAboutFish "hat"
+    text <| toString <| findPerson "Legolas" people
     
