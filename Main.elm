cf7b0465e27c4bde7820f269cbb1e4271c18e67f
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-04-16 15:41:49 +0200

    elm-create-apps-using-the-elm-application-architecture

--- Main.elm
+++ Main.elm
@@ -1,7 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (beginnerProgram)
+import Html.Events exposing (..)
+-- import Html.Attributes exposing (..)
 -- import List
 
 -- politely : String -> String
@@ -97,16 +99,43 @@ import Html.Attributes exposing (..)
 --         , ul [] (List.map renderShip ships)
 --         ]
 
-numbers =
-    [1, 2, 3, 4, 5 ]
+-- numbers =
+--     [1, 2, 3, 4, 5 ]
 
-printThing : thing -> Html msg
-printThing thing =
-    ul [] [text <| toString thing ]
+-- printThing : thing -> Html msg
+-- printThing thing =
+--     ul [] [text <| toString thing ]
 
-fruits =
-    [ { name = "Orange" }, { name= "Banana" } ]
+-- fruits =
+--     [ { name = "Orange" }, { name= "Banana" } ]
+
+-- Four Parts
+
+model =
+    { showFace = False }
+
+type Msg =
+    ShowFace
+
+update msg model_ =
+    case msg of
+        ShowFace -> { model_ | showFace = True }
+
+view model_ =
+    div []
+        [ h1 [] [ text "Face generator" ]
+        , button [onClick ShowFace ] [ text "Face me" ]
+        , if model_.showFace then
+            text "ᕕ( ᐛ )ᕗ"
+            else
+                text ""
+
+        ]
 
 main = 
-    ul [] (List.map printThing fruits)
+    beginnerProgram
+        { model = model
+        , update = update 
+        , view = view
+        }
     
